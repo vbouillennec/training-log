84eab7d0a70ef9a4e98e3d41ca417952e753e8b1
vbouillennec <val.bouillennec@gmail.com>
2022-04-23 22:46:13 +0200

    deploy CRUD interactions

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,6 +7,7 @@ export const environment = {
 	storageBucket: "training-log-e8c2d.appspot.com",
 	messagingSenderId: "784940770700",
 	appId: "1:784940770700:web:8200a9868b9af0f71222a9",
-	measurementId: "G-09ZMXV22F0"
+	measurementId: "G-09ZMXV22F0",
+	databaseURL: "https://training-log-e8c2d-default-rtdb.europe-west1.firebasedatabase.app",
   }
 };
